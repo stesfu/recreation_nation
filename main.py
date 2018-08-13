8e110be417c114d6e31875a8eb27559b890f99f5
Salem Tesfu <salemtesfu@gmail.com>
2018-08-13 11:20:04 -0700

    test

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ jinja_env = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-class MainHandler(webapp2.RequestHandler):
+#class MainHandler(webapp2.RequestHandler):
+    #def get(self):
+        #self.response.write("Welcome to Recreation Nation")
+
+class WelcomeHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write("Welcome to Hogwarts' Online Portal")
+        welcome_template = jinja_current_directory.get_template(
+            "templates/welcome.html")
+        self.response.write(welcome_template.render())
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
+    ('/welcome', WelcomeHandler),
 ], debug=True)
